9c6e3d6ba6acc09de43fbf856778ef3b3921965d
Ausduo <47653014+Ausduo@users.noreply.github.com>
2021-08-07 13:08:34 +1000

    Add files via upload

---
+++ french toast.py
@@ -0,0 +1,10 @@
+print ("french toast")  
+bread = input ("enter bread size - thick or thin?")
+bread = bread.lower ()
+if bread == "thick":
+    print ("dunk the bread a long time")
+elif bread == "thin":
+    print ("dunk the bread quickly")
+else: 
+    print ("don't do anything with it then")
+print ("Thanks for following the help guide")  
